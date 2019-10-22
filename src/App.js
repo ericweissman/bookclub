58baed4e372fa654999dd350320c4567a85e8291
Eric Weissman <eric.weissman21@gmail.com>
2019-10-22 13:26:16 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { books } from './list.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books
+    }
+  }
+
+  random = () => {
+    return this.state.books.filter(book => !book.read).splice(0, 3);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
 }
 
 export default App;
