53398bbf34ed8cb9ddb82e50c1eac65b049bd350
Eric Weissman <eric.weissman21@gmail.com>
2019-10-24 14:31:37 -0600

    Add initial Info functionality

---
+++ src/Info/Info.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../Info/Info.scss'
+
+
+const Info = (props) => {
+  const {title, author, description, id, pages, published, stars, short_desc, remove, img, showInfo} = props;
+  return ReactDOM.createPortal(
+    <div className={showInfo ? 'modal' : 'hidden'}>
+      <div className="info">
+        <h3>{title}</h3>
+        <h4>by {author}</h4>
+        <div className="basic-info">
+          <p>{pages} pages</p>
+          <p>published {published}</p>
+          <p>{stars} stars</p>
+        </div>
+        <div className="descriptions">
+          <p>{short_desc}</p>
+          <p>{description}</p>
+        </div>
+        <button name="showInfo" onClick={(event) => props.handleChange(event)}>Go Back</button>
+      </div>
+    </div>,
+    document.getElementById('portal-root')
+  )
+}
+
+
+export default Info;
