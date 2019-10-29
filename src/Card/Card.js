2e38c3643ac869434109f17dee0dc7b14f9e3998
Eric Weissman <eric.weissman21@gmail.com>
2019-10-29 15:19:23 -0600

    Convert to functional comp with hooks

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,46 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../Card/Card.scss'
 import Info from '../Info/Info.js'
 
-class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showInfo: false,
-      showDesc: false,
-      showInfo: false,
-    }
-  }
+const Card = (props) => {
+  const [showDesc, setShowDesc] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
+  const {title, author, description, id, pages, published, stars, short_desc, remove, img} = props;
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { name } = event.target
-    this.setState({
-      [name]: !this.state[name]
-    });
+    if (name === 'showDesc') {
+      showDesc(!showDesc)
+    } else {
+      setShowInfo(!showInfo)
+    }
   };
 
-render() {
-  const {title, author, description, id, pages, published, stars, short_desc, remove, img} = this.props;
-  const {showInfo, showDesc} = this.state;
   const style = {
     backgroundImage: `url(${img})`
   }
+
   return (
     <div key={id} className="book-card" style={style}>
       <div>
-        <button name="showInfo" onClick={(event) => this.handleChange(event)}>Show Info</button>
+        <button name="showInfo" onClick={(event) => handleChange(event)}>Show Info</button>
         <button onClick={() => remove(id)}>BOO!</button>
       </div>
 
       <Info
-        {...this.props}
-        handleChange={this.handleChange}
+        {...props}
+        handleChange={handleChange}
         showInfo={showInfo}
       />
     </div>
     )
   }
-}
 
 
 export default Card;
